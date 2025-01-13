25945da4995c83bae3219c73d94556e1807515e7
Mohamed D <simoforever2018@gmail.com>
2025-01-13 13:52:39 +0100

    api complete: merging all branches

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express from "express";
-import helmet from "helmet"
-import config from "@/config/config";
-import authService from "@/services/auth/routes"
+import helmet from "helmet";
+import authService from "@/services/auth/routes";
 import portfolioService from "@/services/portfolio/routes";
 import { ensureToken } from "./services/auth/middlewares";
-import cryptoService from "@/services/crypto/routes"
-import portfolioItemService from "@/services/portfolio-item/routes"
+import cryptoService from "@/services/crypto/routes";
+import portfolioItemService from "@/services/portfolio-item/routes";
+import config from "./config/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(helmet());
 
@@ -20,6 +30,6 @@ app.use("/portfolio", portfolioService);
 app.use("/crypto", cryptoService);
 app.use("/portfolios/:id/items", portfolioItemService);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(config.server.port, () => {
+  console.log(`Server is running on port ${config.server.port}`);
 });
